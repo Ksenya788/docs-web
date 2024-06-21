3b9590db7bd74a9bc66e118ed1e6efc97e767773
Alevar <chupick228@gmail.com>
2024-06-21 19:42:45 +0300

    feat: style and make some pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,8 +3,8 @@ import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: "My Site",
-  tagline: "Dinosaurs are cool",
+  title: "Откуда Подписки Бот",
+  tagline: "Простое и исчерпывающее объяснение закроет все вопросы за 10 минут",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
@@ -35,10 +35,6 @@ const config: Config = {
       {
         docs: {
           sidebarPath: "./sidebars.ts",
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         theme: {
           customCss: "./src/css/custom.css",
@@ -61,7 +57,7 @@ const config: Config = {
           type: "docSidebar",
           sidebarId: "tutorialSidebar",
           position: "left",
-          label: "Tutorial",
+          label: "Документация",
         },
       ],
     },
@@ -72,8 +68,8 @@ const config: Config = {
           title: "Docs",
           items: [
             {
-              label: "Tutorial",
-              to: "/docs/intro",
+              label: "Документация",
+              to: "/docs/возможности",
             },
           ],
         },
@@ -97,10 +93,6 @@ const config: Config = {
         {
           title: "More",
           items: [
-            {
-              label: "Blog",
-              to: "/blog",
-            },
             {
               label: "GitHub",
               href: "https://github.com/facebook/docusaurus",
